df2ffa16d0e106c7dbd4ec56c36ae304dc67f6f4
Appon Islam <appon2003@gmail.com>
2025-05-09 01:27:39 +0600

    auth problem fixed

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { authController } from "./auth.controller";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/login", authController.loginUser);
+router.get("/me", auth(), authController.getMe);
 
 export const authRoute = router;
